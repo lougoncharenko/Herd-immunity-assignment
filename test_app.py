e9314c108aa30762cfbdd4fee79af9fa2f9ad961
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-29 15:58:35 -0500

    created tests for virus class

--- test_app.py
+++ test_app.py
@@ -13,3 +13,20 @@ import pytest
 import io
 import sys
 from virus import Virus
+
+#test Virus class
+def test_virus_instance():
+    virus = Virus("HIV", 0.8, 0.3)
+    assert virus
+
+def test_virus_name():
+    virus = Virus("HIV", 0.8, 0.3)
+    assert virus.name == "HIV"
+
+def test_virus_repro_rate():
+     virus = Virus("HIV", 0.8, 0.3)
+     assert virus.repro_rate == 0.8
+
+def test_virus_mortality_rate():
+    virus = Virus("HIV", 0.8, 0.3)
+    assert virus.mortality_rate == 0.3
