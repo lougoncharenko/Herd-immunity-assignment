d8d6ef8806d1917dff98f3d124ef82ba562143f1
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-29 15:49:01 -0500

    initialized test file

---
+++ test_app.py
@@ -0,0 +1,15 @@
+################################################################################
+## To run the tests, first make sure you have pytest installed:
+##
+## $ pip3 install pytest
+##
+## Then, open up a terminal to the homework folder and run the following:
+##
+## $ pytest
+##
+################################################################################
+
+import pytest
+import io
+import sys
+from virus import Virus
