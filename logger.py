1f61ee7b19a2158f8eb7bc9758137c2bddc0fca0
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-03 09:38:21 -0500

    finished log_infection_survival method

--- logger.py
+++ logger.py
@@ -17,7 +17,7 @@ class Logger(object):
         """
         filename = open(self.file_name, "w")
         filename.write(
-            f"\t Population Size: {pop_size}, \t Vaccination Percentage: {vacc_percentage}, \t Virus: name {virus_name}, \t mortality rate: {mortality_rate}, \t basic reproduction number: {basic_repro_num} /n"
+            f"\t Population Size: {pop_size} \t Vaccination Percentage: {vacc_percentage} \t Virus: name {virus_name} \t mortality rate: {mortality_rate} \t basic reproduction number: {basic_repro_num} \n"
         )
         filename.close()
 
@@ -26,21 +26,25 @@ class Logger(object):
         This method logs the number of interactions, number of new infections and steps
         """
         filename = open(self.file_name, 'a')
-        filename.write(f'Step number: {step_number}\nInteractions: {number_of_interactions}\nNew Infections: {number_of_new_infections}\n')
+        filename.write(f '\t Step number: {step_number} \t Interactions: {number_of_interactions} \t New Infections: {number_of_new_infections}\n')
+        filename.close()
 
     def log_infection_survival(self, step_number, population_count, number_of_new_fatalities):
         """
         method that logs if the person survived, currrent population count and number of new fatalities
         """
-        # TODO: Finish this method. If the person survives, did_die_from_infection
-        # should be False.  Otherwise, did_die_from_infection should be True.
-        # Append the results of the infection to the logfile
-        pass
+        filename = open(self.file_name, "a")
+        death_total = 0
+        for person in number_of_new_fatalities
+             if not person.is_alive:
+                 death_total += 1
+        file_name.write(f"  \t Step Number: {step_number}  \t Population count: {population_count}  \t  Death Total: {death_total} \n")
+        filename.close()
 
     def log_time_step(self, time_step_number):
         """
         This method logs time step number
         """
         filename = open(self.file_name, "a")
-        filename.write(f"Time Step Number: {time_step_number}")
+        filename.write(f" \t Time Step Number: {time_step_number} \n")
         filename.close()       
